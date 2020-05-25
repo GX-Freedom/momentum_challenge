999e98bbdff6695a5f8bc79da539885d885a2340
GX-Freedom <kjy911206@gmail.com>
2020-05-25 18:33:27 +0900

    update css and background.js
    update css and background.js

--- background.js
+++ background.js
@@ -1,28 +1,35 @@
 
 const body = document.querySelector("body");
-
+const html = document.querySelector("html");
 const IMG_NUMBER = 12;
-
 
 function paintImage(IMG_NUMBER) {
     const image = new Image();
-    image.src = `./images/${genRandom() + 1}.jpg`;
+    image.src = `./images/${getRandom() + 1}.jpg`;
     image.classList.add("backgroundimg");
+    //image.style.height = "100px"
+    image.height = body.scrollHeight;
     body.appendChild(image);
 }
 
-function genRandom() {
+function getRandom() {
     const number = Math.floor(Math.random() * IMG_NUMBER);
-
     return number;
 }
 
 
 
 function init() {
-    const randomNumber = genRandom();
+
+    const randomNumber = getRandom();
     paintImage(randomNumber);
     window.addEventListener("click", init);
+
+    let img = document.getElementsByClassName("backgroundimg");
+
+    if (img.length > 1) {
+        img[0].remove();
+    }
 }
 
 init();
